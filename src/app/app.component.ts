d95205ce8f55d84fdf592956c7b1c7fea95744a7
crisRR <140411058+crisrdguez@users.noreply.github.com>
2023-07-31 16:18:23 +0200

    mejorando funcionalidad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import * as math from 'mathjs'; // Importa math.js
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit{
   title = 'Proyecto Calculadora';
 
   screen = "0"; //[(ngModel)]="screen"
   currentValue='';
+  ultimoInputOperador = false;
 
-  //funcion
-  displayNumber(value:string){
-    this.currentValue = this.currentValue + value;
-    this.screen = this.currentValue;
+
+  displayNumber(value: string) {
+    if (this.screen === '0' || this.screen === 'Error') {
+      this.screen = value;
+    } else {
+
+        this.screen += value;
+    }   
+
+        this.ultimoInputOperador=false;
   }
 
-  /*
-  equals(){
-    this.screen = eval(this.currentValue);
-    this.currentValue=this.screen;
-  }*/
+  displayOperator(value: string) {
+    if (!this.ultimoInputOperador && this.screen !== 'Error') {
+      this.screen += value;
+      this.ultimoInputOperador = true;
 
+    }
+  }
+
+  
+
+  //evalua la expresion y muestra el resultado
   equals() {
     try {
-      this.screen = eval(this.currentValue); // Utilizamos el servicio eval() de JavaScript
-      this.currentValue = this.screen.toString();
+      this.currentValue = this.screen;
+      const result = math.evaluate(this.screen);
+      this.screen = result.toString();
+
+      //Agregamos la operacion y el resultado al historial
+    const operation = this.currentValue + ' = ' + result;
+    this.operationHistory.push(operation);
+
+    // Reiniciar currentValue para una nueva operación
+    this.currentValue = '';
+
+    this.ultimoInputOperador=false;
+
+
+
     } catch (error) {
       this.screen = 'Error';
       this.currentValue = '';
+      this.ultimoInputOperador=false;
     }
   }
 
@@ -37,22 +65,42 @@ export class AppComponent implements OnInit{
   clear(){
     this.currentValue='';
     this.screen='0';
+    this.ultimoInputOperador=false;
   }
 
-  back(){
-    this.currentValue = this.currentValue.slice(0,-1);
-    this.screen=this.currentValue;
-    if(this.currentValue==''){
-      this.screen='0';
+
+  // Elimina el ultimo caracter de la pantalla
+  back() {
+    if (this.screen !== 'Error') {
+      this.screen = this.screen.slice(0, -1) || '0';
+      this.ultimoInputOperador = this.esUltCaracterOperador();
     }
-    
   }
 
-  calcvalue(solve:any){
-    if(solve.charAt(0)=='0'){
-      solve = solve.slice(1,);
+  /**La función includes() se utiliza para verificar si un elemento está presente en un array. 
+   * En este caso, se verifica si el último carácter de this.screen está presente en el array operators. 
+   * Si es así, significa que el último carácter es un operador matemático, y la función devolverá true. Si no, devolverá false. */
+  esUltCaracterOperador() {
+    const operators = ['+', '-', '*', '/'];
+    return operators.includes(this.screen.slice(-1));
+  }
+
+  calcvalue(solve: any) {
+    try {
+      const result = math.evaluate(solve);
+      this.screen = result.toString();
+    } catch (error) {
+      this.screen = 'Error';
     }
-    this.screen = eval(solve);
+    this.ultimoInputOperador=false;
+  }
+
+
+  //Historial de operaciones
+  operationHistory:string[]=[];
+
+  clearHistory(){
+    this.operationHistory = [];
   }
 
 
